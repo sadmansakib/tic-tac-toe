19cece7fd706f7287c90c7a552d99a2ad915dea5
sadmansakib <ssadman8@gmail.com>
2022-03-21 13:04:51 +0600

    POJO class for movement coordinates on board
    Signed-off-by: sadmansakib <ssadman8@gmail.com>

---
+++ src/main/java/com/sadmansakib/tictactoe/entity/Coordinates.java
@@ -0,0 +1,11 @@
+package com.sadmansakib.tictactoe.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Coordinates {
+    private int x;
+    private int y;
+}
